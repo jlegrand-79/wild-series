ed6dc0b0f531010ff18985ae35434baa8b27e149
Jérôme Legrand <jerome.legrand.79@gmail.com>
2022-11-29 15:59:56 +0100

    Symfony Quest Nr.6 Challenge

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -6,14 +6,26 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route('/program', name: 'program_')]
 class ProgramController extends AbstractController
 {
-    #[Route('/program/', name: 'program_index')]
+    #[Route('/', name: 'index')]
 
     public function index(): Response
     {
         return $this->render('program/index.html.twig', [
             'website' => 'Wild Series',
+        ]);
+    }
+
+
+    // #[Route('/{id}', methods: ['GET'], requirements: ['id' => '\d+'], name: 'show')]
+    #[Route('/{id<\d+>}', name: 'show', methods: ['GET'])]
+    // Alternative : il est aussi possible d'écrire les requirements de manière plus condensée : #[Route('/program/list/{page<\d+>}', name: 'program_list')]. Cette écriture inline nécessite l'utilisation des chevrons < > pour encadrer la regex.
+    public function show(int $id): Response
+    {
+        return $this->render('program/show.html.twig', [
+            'id' => $id,
         ]);
     }
 }
