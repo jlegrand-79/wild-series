e500ab80f83c85fe44f9539c2f8fb9ab5eae10bf
Jérôme Legrand <jerome.legrand.79@gmail.com>
2022-12-04 22:15:31 +0100

    Symfony Quest Nr.11 Challenge

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Repository\ProgramRepository;
+use App\Repository\SeasonRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -35,6 +36,30 @@ class ProgramController extends AbstractController
         }
         return $this->render('program/show.html.twig', [
             'program' => $program,
+        ]);
+    }
+
+    #[Route('/{programId<^[0-9]+$>}/season/{seasonId<^[0-9]+$>}', name: 'season_show')]
+    public function showSeason(int $programId, int $seasonId, ProgramRepository $programRepository, SeasonRepository $seasonRepository): Response
+    {
+        $program = $programRepository->findOneBy(['id' => $programId]);
+        $season = $seasonRepository->findOneBy(['id' => $seasonId]);
+
+        if (!$program) {
+            throw $this->createNotFoundException(
+                'No program with id : ' . $programId . ' found in program\'s table.'
+            );
+        }
+
+        if (!$season) {
+            throw $this->createNotFoundException(
+                'No season with id : ' . $seasonId . ' found in season\'s table.'
+            );
+        }
+
+        return $this->render('program/season_show.html.twig', [
+            'program' => $program,
+            'season' => $season,
         ]);
     }
 }
--- src/DataFixtures/CategoryFixtures.php
+++ src/DataFixtures/CategoryFixtures.php
@@ -14,6 +14,7 @@ class CategoryFixtures extends Fixture
         'Animation',
         'Fantastique',
         'Horreur',
+        'Comédie',
     ];
 
     public function load(ObjectManager $manager)
